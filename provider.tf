b76106d1a8c6f1e63f4b97a217015cbabc9fbc7c
ravibangaram <ravikumar_prathi@hotmail.com>
2021-02-16 18:17:53 -0700

    pat changed

--- provider.tf
+++ provider.tf
@@ -5,6 +5,6 @@ terraform {
 }
 
 provider "github" {
-  organization = "wahlnetwork"
+  organization = "ravibangaram"
   token        = var.github_token
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "github_token" {
   type        = string
-  description = "5cba96ea0aac3b6201ac12b7b84a08447d80f7a3"
+  description = "cc32c5efa38b1d38f6867bed68f14aed81756fd4"
 }
